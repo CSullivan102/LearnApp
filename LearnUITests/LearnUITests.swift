14247a1603a1d5417d8bd64ebacc149263804dc0
CSullivan102 <CSullivan102@gmail.com>
2015-08-18 17:37:26 -0400

    Initial Commit

---
+++ LearnUITests/LearnUITests.swift
@@ -0,0 +1,34 @@
+//
+//  LearnUITests.swift
+//  LearnUITests
+//
+//  Created by Christopher Sullivan on 8/18/15.
+//  Copyright © 2015 Christopher Sullivan. All rights reserved.
+//
+
+import XCTest
+
+class LearnUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
